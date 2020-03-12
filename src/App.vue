17177e8970652dcbacc34c98cf15b0aa9a06319f
Emerson Duarte De Amorim <560766@sga.pucminas.br>
2020-03-12 20:30:45 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form @submit="onFormSubmit">
+      <p>
+        <label>First name:</label>
+        <input type="text" v-model="firstName">
+        {{firstNameError}}
+      </p>
+      <p>
+        <label>Last name:</label>
+        <input type="text" v-model="lastName">
+      </p>
+      <p>
+        <label>E-mail:</label>
+        <input type="text" v-model="email">
+      </p>
+      <button type="submit">Send</button>
+    </form>
+    <p>{{firstName}} {{lastName}} | {{email}}</p>
+
+    <div>
+      <ul>
+        <li v-for="user in users" :key="user.name">{{user.name}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      email: "",
+      firstNameError: "",
+      users: []
+    };
+  },
+  watch: {
+    firstName() {
+      if (this.firstName.length <= 5) {
+        this.firstNameError = "Nome precisa conter mais do que 5 caracteres";
+      } else {
+        this.firstNameError = "";
+      }
+    }
+  },
+  methods: {
+    onFormSubmit(e) {
+      e.preventDefault();
+
+      if (this.firstNameError !== "") {
+        return;
+      }
+
+      this.users = this.users.concat({
+        name: `${this.firstName}  ${this.lastName}`,
+        email: this.email
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
